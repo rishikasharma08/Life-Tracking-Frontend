498111ab8a679804c30432e090e2728ae562fe00
rishikasharma08 <rishikasharma7816@gmail.com>
2022-05-14 23:59:48 +0530

    Added another files

--- src/app/routes/pages/home/home.component.ts
+++ src/app/routes/pages/home/home.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  showMain: boolean = true;
+  showWealth: boolean = false;
+  showHealth: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  wealthCards() {
+    this.showMain = false;
+    this.showWealth = true;
+  }
+  healthCards() {
+    this.showMain = false;
+    this.showHealth = true;
+  }
+  chooseAgain() {
+    this.showMain = true;
+    this.showHealth = false;
+    this.showWealth = false;
+  }
 }
