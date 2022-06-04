293dbfb00854d6daa8f3f715f3acfcfc5e5e1c6c
rishikasharma08 <rishikasharma7816@gmail.com>
2022-06-04 11:46:08 +0530

    Multiple changes

--- src/app/routes/health-track/water-track/water-track.component.ts
+++ src/app/routes/health-track/water-track/water-track.component.ts
@@ -8,11 +8,43 @@ import { MainserviceService } from 'src/app/core/services/mainservice.service';
 })
 export class WaterTrackComponent implements OnInit {
 
+  waterData: any;
+  user_id: any;
+  percentage: any;
   constructor(private service: MainserviceService) { }
 
   ngOnInit(): void {
+    this.getWaterData();
+  }
+
+  getWaterData() {
+    this.user_id = localStorage.getItem('userId');
+    this.service.api({ user_id: this.user_id }, `life_tracking/get_water_data`, 200, 'post')
+      .subscribe((res) => {
+        if (res.error == 0) {
+          this.waterData = res.data;
+          this.percentage = (this.waterData[0].intake_glasses / this.waterData[0].required_glasses) * 100;
+        }
+        else {
+          this.service.customPopups(res.msg, 1);
+        }
+      })
   }
   logout() {
     this.service.logout();
+  }
+
+  drunkWater() {
+    this.service.api({ user_id: this.user_id }, `life_tracking/update_water_data`, 200, 'post')
+      .subscribe((res) => {
+        if (res.error == 0) {
+
+          this.service.customPopups(res.msg, 0);
+          this.getWaterData();
+        }
+        else {
+          this.service.customPopups(res.msg, 1);
+        }
+      })
   }
 }
