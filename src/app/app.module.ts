39b951a5b0fbfd834e83a4b15f5602244be9d560
rishika08 <rishikasharma7816@gmail.com>
2022-06-17 21:07:23 +0530

    expense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoutesModule } from './routes/routes.module';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
--- src/app/routes/expense-track/expense-track.component.ts
+++ src/app/routes/expense-track/expense-track.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MainserviceService } from 'src/app/core/services/mainservice.service';
+// import { convertJsToTs, convertJsToTsSync } from 'js-to-ts-converter';
+
+// convertJsToTs('expense.js');
+// console.log('Done');
+
 
 @Component({
   selector: 'app-expense-track',
@@ -11,6 +16,7 @@ export class ExpenseTrackComponent implements OnInit {
   constructor(private service: MainserviceService) { }
 
   ngOnInit(): void {
+    
   }
   logout() {
     this.service.logout();
